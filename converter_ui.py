0fed7a1fa37c38ea674fe1ae2958ddce4518d8d7
Karczel <92095164+Karczel@users.noreply.github.com>
2024-02-13 14:35:03 +0700

    Update converter_ui.py

--- converter_ui.py
+++ converter_ui.py
@@ -34,13 +34,13 @@ class ConverterUI(tk.Tk):
         self.rightinput = tk.StringVar()
 
         # left input field
-        leftfield = tk.Entry(self, width=20, textvariable=self.leftinput)
+        self.leftfield = tk.Entry(self, width=20, textvariable=self.leftinput)
         # combobox
         self.leftunit, self.leftchooser = self.load_units(self.unit_type,self.do_nothing)
         # label '='
         label = tk.Label(self, text="=")
         # right input field
-        rightfield = tk.Entry(self, width=20, textvariable=self.rightinput)
+        self.rightfield = tk.Entry(self, width=20, textvariable=self.rightinput)
         # combobox
         self.rightunit, self.rightchooser = self.load_units(self.unit_type, self.do_nothing)
         # convert button
@@ -49,23 +49,23 @@ class ConverterUI(tk.Tk):
         clear_button = tk.Button(self, text="Clear", command=self.clear_handler)
 
         # Enter bind
-        leftfield.bind('<Return>', self.convert_handler)
-        rightfield.bind('<Return>', self.convert_handler)
+        self.leftfield.bind('<Return>', self.convert_handler)
+        self.rightfield.bind('<Return>', self.convert_handler)
         unitchooser.bind('<<Combobox>>', self.update_unit)
 
         # layout
         padding = {'padx': 10, 'pady': 10}
         # position & size the components
         unitchooser.pack(**padding, anchor=tk.NW)
-        leftfield.pack(side=tk.LEFT, **padding, expand=True, fill=tk.X)
+        self.leftfield.pack(side=tk.LEFT, **padding, expand=True, fill=tk.X)
         self.leftchooser.pack(side=tk.LEFT, **padding, expand=True, fill=tk.X)
         label.pack(side=tk.LEFT, **padding)
-        rightfield.pack(side=tk.LEFT, **padding, expand=True, fill=tk.X)
+        self.rightfield.pack(side=tk.LEFT, **padding, expand=True, fill=tk.X)
         self.rightchooser.pack(side=tk.LEFT, **padding, expand=True, fill=tk.X)
         clear_button.pack(side=tk.RIGHT, **padding)
         convert_button.pack(side=tk.RIGHT, **padding)
 
-    def load_units(self, unittype: UnitType, function):
+    def load_units(self, unittype, function):
         """Load units of the requested unittype into the comboboxes."""
         units = self.converter.get_units(unittype)
         selected = tk.StringVar()
@@ -102,6 +102,11 @@ class ConverterUI(tk.Tk):
         except ValueError:
             messagebox.showerror(message="Only number input are accepted")
             # change to red input and clear other, return to black when re-input
+            if self.leftinput != "":
+                self.leftfield.configure(fg='red')
+                self.rightinput.set("")
+            else:
+                self.leftinput.set("")
 
     def do_nothing(self, *args):
         pass
